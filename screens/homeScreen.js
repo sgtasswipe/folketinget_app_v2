a94ff00f18d04c747397d01962ab56a36ba7ad4f
sgtasswipe <otro0001@stud.kea.dk>
2025-11-18 15:04:31 +0100

    updated to v 54 of expo sdk

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,9 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
+import { use, useState, useEffect } from 'react';
 
 export default function HomeScreen() {
+const [apiData, setApiData] = useState([])
+const [page, setPage] = useState(0)
+const pageSize = 20;
+const [searchQuery, setSearchQuery] = useState("")
+const fetchApi = async () => {
+ const apiUrl = `https://oda.ft.dk/api/Afstemning?$inlinecount=allpages&$orderby=opdateringsdato desc&$skip=${
+        page * pageSize
+      }&$top=${pageSize}&$expand=Sagstrin,Sagstrin/Sag &$filter=(typeid eq 1 or typeid eq 3)`
+      response = await fetch(apiUrl)
+      const extractedData = await response.json()
+      setApiData(extractedData)
+      console.log(extractedData)
+    }
+        useEffect(() => {
+    fetchApi(); 
+  }, [page]); 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Pressable onPress={fetchApi} >
+        <Text>Fetch Data: Press! </Text>
+      </Pressable>
     </View>
   );
 }

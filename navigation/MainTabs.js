3b565c0e686790be46a5f4685ce096bed908bdff
HasanRashid97 <hasanrashid97@gmail.com>
2025-12-03 13:26:15 +0100

    naming fix for votedetails

--- navigation/MainTabs.js
+++ navigation/MainTabs.js
@@ -1,16 +1,19 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/homeScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import VoteDetailsScreen from '../screens/VoteDetailsScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/homeScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import VoteDetailsScreen from "../screens/VoteDetailsScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 const Tab = createBottomTabNavigator();
 const HomeStack = createNativeStackNavigator();
 
-
-function HomeStackScreen (){
- return (
+function HomeStackScreen() {
+  return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="VotesList" component={HomeScreen} options={{ headerShown: false }} />
+      <HomeStack.Screen
+        name="VotesList"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
       <HomeStack.Screen name="VoteDetails" component={VoteDetailsScreen} />
     </HomeStack.Navigator>
   );
@@ -19,8 +22,8 @@ function HomeStackScreen (){
 export default function MainTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStackScreen} />    
-      <Tab.Screen name="Profile" component={ProfileScreen}/>
+      <Tab.Screen name="Home" component={HomeStackScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
-  ) }
-  
+  );
+}
